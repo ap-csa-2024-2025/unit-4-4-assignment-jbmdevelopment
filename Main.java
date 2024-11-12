95a401911c01b8997c6e79067dcc1d928757eebd
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-11-12 19:00:12 +0000

    aaaa

--- Main.java
+++ Main.java
@@ -5,9 +5,22 @@ public class Main
   public static void main(String[] args)
   {
     // You can test your method here
-     System.out.println(isPrime(1));
+     /*System.out.println(isPrime(1));
      System.out.println(isPrime(4));
-     System.out.println(isPrime(17));
+     System.out.println(isPrime(17));*/
+     Scanner input = new Scanner(System.in);
+     System.out.println("Enter a string");
+     String msg = input.nextLine();
+     int counts = 0;
+     msg = msg.toLowerCase();
+     for(int i = 0; i < msg.length() - 1; i++) { // traverse two letters zt a time
+        String second = msg.substring(i, i + 2); 
+        boolean condition = (second.equals("pa") || second.equals("po") || second.equals("pu") || second.equals("pe") || second.equals("pi"));
+        if(condition) {
+            counts++;
+        }
+     }
+     System.out.println("Contains p followed by a vowel " + counts + " times");
   }
 
   public static boolean isPrime(int N)
