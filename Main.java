b51526a9c8f43a8d0d3132040e35681dd209e824
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-11-12 18:42:33 +0000

    a

--- Main.java
+++ Main.java
@@ -5,12 +5,25 @@ public class Main
   public static void main(String[] args)
   {
     // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
+     System.out.println(isPrime(1));
+     System.out.println(isPrime(4));
+     System.out.println(isPrime(17));
   }
 
   public static boolean isPrime(int N)
   {
+    int counter = 0;
+    if(N == 0) {
+      return false;
+    }
+    for(int i = N; i > 0; i--) {
+       if(N % i == 0) {
+          counter++;
+       }
+    }
+    if(counter == 2) {
+      return true;
+    }
     // TODO: Replace and write your code below
     return false;
   }
